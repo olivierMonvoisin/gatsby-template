b54bfc14bc2308cd8270775e0dcdffab144f4083
Olivier Monvoisin <oliviermonvoisin@gmail.com>
2018-07-25 15:32:53 -0400

    Add Styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components';
 
 import Layout from '../components/layout'
 
+const Title = styled.h1`
+  color: palevioletred;
+`;
+
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
+    <Title>Hi people</Title>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
