7ee91250600dbc2904ba11e1c4e1f924bd82efca
Olivier Monvoisin <oliviermonvoisin@gmail.com>
2018-07-31 20:49:39 -0400

    Fix transformer

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,12 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [],
+      },
+    },
+    {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
