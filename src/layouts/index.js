bf890172630469862fcee1fd95abc125fb164ae8
Olivier Monvoisin <oliviermonvoisin@gmail.com>
2018-08-01 11:32:17 -0400

    Remove sass Add styled-component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
 
 import Navbar from '../components/Navbar'
-import './all.sass'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Home | Gatsby + Netlify CMS YEAH" />
     <Navbar />
+    <Title>TITRE OLI</Title>
     <div>{children()}</div>
   </div>
 )
